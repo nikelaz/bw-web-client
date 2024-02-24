9c076eea39f7395bb4bec3befe008e984ad81a82
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2024-02-24 17:48:14 +0200

    remove unnecessary code/files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 300,
+    },
+  }),
+};
 
 export default nextConfig;
