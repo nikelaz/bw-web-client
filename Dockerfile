7fb2fa516667a6ec6ad52e353d190d1fbd9e5c6c
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2024-08-23 00:19:48 +0300

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ COPY package*.json ./
 RUN npm install
 COPY . ./
 RUN npm run build
+COPY . ./
 EXPOSE 80
 CMD [ "next", "start" ]
--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,9 @@ const nextConfig = {
       },
     ]
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
