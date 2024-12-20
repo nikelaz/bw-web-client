1c9523cd9fcd088c4f46839b85a496f1e66554c8
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2024-12-20 11:54:42 +0200

    fix default current month opening next month after 50% of the month is finished (due to faulty logic)

--- src/helpers/find-closest-budget-date.ts
+++ src/helpers/find-closest-budget-date.ts
@@ -3,10 +3,16 @@ import type { Budget } from '@/types/budget';
 const findClosestBudgetDate = (referenceDate: Date, budgets: Array<Budget>): Budget | null => {
   if (budgets.length === 0) return null;
 
+  const currentMonth = new Date().getMonth();
+
   const budgetsCopy = budgets.map(budget => ({
     ...budget,
     month: new Date(budget.month),
   }));
+
+  const exactBudget = budgetsCopy.find(budget => budget.month.getMonth() === currentMonth);
+
+  if (exactBudget) return exactBudget;
 
   budgetsCopy.sort((a: any, b: any) => {
     const distancea = Math.abs(referenceDate.getTime() - a.month);
